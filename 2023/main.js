5ebe16fb2cb8139a31a37526cd05d09815831202
FKappel <you@example.com>
2023-12-06 21:39:53 +0100

    Read input

--- 2023/main.js
+++ 2023/main.js
@@ -1,15 +1,20 @@
-function CopyToClipboard()
-{
+
+
+function CopyToClipboard(){
 var r = document.createRange();
-r.selectNode(document.getElementById('Output'));
+r.selectNode(document.getElementById('OutputP1'));
 window.getSelection().removeAllRanges();
 window.getSelection().addRange(r);
 document.execCommand('copy');
 window.getSelection().removeAllRanges();
 }
 
-function writeOutput(outText){
-    document.getElementById('Output').innerHTML = outText
+function writeOutput(outText, part){
+    if(part == 1){
+        document.getElementById('OutputP1').innerHTML = outText
+    }else if(part == 2){
+        document.getElementById('OutputP2').innerHTML = outText
+    }
 }
 
 function getInput(day){
@@ -22,21 +27,40 @@ function doDayx_(){
     writeOutput()
 }
 */
-function doDay1_1(){
+function manageFileDrop(content, fileName){
+    const arrContent = content.split(/\r?\n|\r|\n/g)
+    const DayRegex = /(\d+)/g
+    const day = fileName.match(DayRegex)
+    switch(day[0]){
+        case "1":
+            doDay1_1(arrContent)
+            doDay1_2(arrContent)
+            break;
+        case "2":
+            doDay2_1(arrContent)
+            //doDay2_2(arrContent)
+            break;
+        default:
+            console.log("unknown day")
+    }
+}
+
+function doDay1_1(input){
     var regex = /\d/g;
     sum = 0
-    
-    input = getInput("1")
+    console.log(input)
+    //input = getInput("1")
     input.forEach(element => {
+        console.log(element)
         const matches = String(element).match(regex);
         const firstDigit = matches[0];
         const lastDigit = matches[(matches.length-1)];
         const lineNumber = Number(firstDigit+lastDigit);
         sum += lineNumber
     });
-    writeOutput(sum)
+    writeOutput(sum, 1)
 }
-function doDay1_2(){
+function doDay1_2(input){
     var regex = /\d|one|two|three|four|five|six|seven|eight|nine/g;
     let NumberMapping = new Map([
         ["one","1"],
@@ -50,7 +74,7 @@ function doDay1_2(){
         ["nine","9"],
     ]);
     sum = 0;
-    input = getInput("1");
+    //input = getInput("1");
     input.forEach(element => {
         const matches = String(element).match(regex);
         var firstDigit = matches[0];
@@ -76,6 +100,52 @@ function doDay1_2(){
         console.log(element + "  -  " + lineNumber)
         sum += lineNumber;
     });
-    writeOutput(sum)
+    writeOutput(sum, 2)
+}
+
+function doDay2_1(input){
+    const rCubes = 12
+    const gCubes = 13
+    const bCubes = 14
+    const inputRegex = /^Game (\d+): (.*)$/
+    const gameRegex = /(\d+) (.*)$/
+    var IDsum = 0
+
+    input.forEach(element =>{
+        let tooHigh = false
+        let countR = 0
+        let countG = 0
+        let countB = 0
+        let matches = String(element).match(inputRegex) 
+        let gameID = Number(matches[1])
+        let drawings = matches[2]
+        let arrDrawings = drawings.split(/,|;/).map(function(item) {
+            return item.trim();
+          });
+        console.log(arrDrawings)
+        arrDrawings.forEach(member => {
+            let matches = member.match(gameRegex)
+            switch(matches[2]){
+                case "green":
+                    countG += Number(matches[1])
+                    break
+                case "blue":
+                    countB += Number(matches[1])
+                    break
+                case "red":
+                    countR += Number(matches[1])
+                    break
+                default:
+            }
+            if(countG > gCubes || countB > bCubes || countR > rCubes){
+                tooHigh = true
+            }
+        });
+        if(!tooHigh){
+            IDsum += gameID
+        }
+
+    })
+    console.log(IDsum)   
 }
 
